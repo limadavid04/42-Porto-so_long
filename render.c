bb7383d95699c4979601f321ba5a45a0f08d1328
limadavid04 <david.04.fsl@gmail.com>
2023-07-13 16:07:16 +0100

    forgot to add this file

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: dlima <dlima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 15:40:37 by dlima             #+#    #+#             */
-/*   Updated: 2023/07/13 15:45:18 by dlima            ###   ########.fr       */
+/*   Updated: 2023/07/13 16:06:32 by dlima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,4 @@ void    floor_move(t_game *game, int i, int j)
 void    pass_over_exit(t_game *game, int i, int j)
 {
 
-}
-void    exit_game(t_game *game, int i, int j)
-{
-    
 }
